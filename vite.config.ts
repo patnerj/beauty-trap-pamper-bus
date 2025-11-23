3ad1ba975152a92fe47b98c75e332cb8cddf0cda
patnerj <35563503+patnerj@users.noreply.github.com>
2025-11-23 12:26:21 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: "/beauty-trap-pamper-bus/",   // 👈 VERY IMPORTANT
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
